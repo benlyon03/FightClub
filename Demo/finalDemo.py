18f8a08bad62294a09081ca094ed65a80621785d
Alex Liang <2alexliang@gmail.com>
2023-12-11 12:22:44 -0800

    Updated paths to files

--- Demo/finalDemo.py
+++ Demo/finalDemo.py
@@ -81,60 +81,6 @@ def numToVal(num):
     return 'Some Very Wrong Is Happening'
 
 
-def record_video(output_path, duration):
-    # Open a video capture object (0 represents the default camera)
-    cap = cv2.VideoCapture(0)
-
-    # Get the width and height of the frames
-    # Dataset dimensions: 244 x 244
-    width = int(cap.get(3))
-    height = int(cap.get(4))
-
-    # Define the codec and create a VideoWriter object
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter(output_path, fourcc, 30, (width, height))
-
-    recording = False
-    start_time = 0
-
-    while True:
-        # Capture frame-by-frame
-        ret, frame = cap.read()
-
-        # Display the frame
-        cv2.imshow('Recording...', frame)
-
-        # Check for 'r' key press to start/stop recording
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord('r'):
-            if not recording:
-                print("Recording started.")
-                start_time = time.time()
-                recording = True
-            else:
-                print("Recording stopped.")
-                recording = False
-
-        # Check if recording and within the specified duration
-        if recording and time.time() - start_time < duration:
-            # Write the frame to the output video
-            out.write(frame)
-        elif recording:
-            print("Recording reached the specified duration.")
-            recording = False
-
-        # Break the loop if 'q' is pressed
-        if key == ord('q'):
-            break
-
-    # Release the VideoCapture and VideoWriter objects
-    cap.release()
-    out.release()
-
-    # Destroy all OpenCV windows
-    cv2.destroyAllWindows()
-
-
 def convert_avi_to_mp4(avi_path, mp4_path, fps=30):
     # Open the AVI file
     cap = cv2.VideoCapture(avi_path)
@@ -164,20 +110,13 @@ def convert_avi_to_mp4(avi_path, mp4_path, fps=30):
 
 if __name__ == "__main__":
     # Set the output video path and recording duration
-    output_avi_path = r'C:\Users\2alex\OneDrive\Documents\GitHub\FightClub\liveVideo.avi'
-    output_mp4_path = r'C:\Users\2alex\OneDrive\Documents\GitHub\FightClub\liveVideo.mp4'
-    recording_duration = 4  # in seconds
 
-    inp = [r"C:\Users\2alex\OneDrive\Documents\GitHub\FightClub\tacobellfight.mp4", r"C:\Users\2alex\OneDrive\Documents\GitHub\FightClub\dance.mp4", r"C:\Users\2alex\OneDrive\Documents\GitHub\FightClub\boxing.mp4", r"C:\Users\2alex\OneDrive\Documents\GitHub\FightClub\running.mp4", r"C:\Users\2alex\OneDrive\Documents\GitHub\FightClub\yoga.mp4", r"C:\Users\2alex\OneDrive\Documents\GitHub\FightClub\brawl.mp4"]
+    # Input video paths for the models
+    inp = ['Demo\Videos\boxing.mp4', 'Demo\Videos\brawl.mp4']
 
     while(True):
 
         for vid in inp:
-        # Start recording
-        #record_video(output_avi_path, recording_duration)
-
-        # Convert AVI to MP4
-        #convert_avi_to_mp4(output_avi_path, output_mp4_path)
             play =  cv2.VideoCapture(vid)
         # Load in model
             while True:
@@ -214,6 +153,7 @@ if __name__ == "__main__":
                     break
             play.release()
 
+            # Loads model from fightnight_iter1.h5. Change it to the path
             model = tf.keras.models.load_model('fightnight_iter1.h5')
 
             # Preprocess input
@@ -224,6 +164,7 @@ if __name__ == "__main__":
             predictions = probability_model.predict(single_vid)
 
             # Load the newly trained model checkpoint
+            # Change it to the paths to epoch_10.pth and config.py
             new_checkpoint = 'epoch_10.pth'
             cfg = 'config.py'
 
@@ -287,11 +228,3 @@ if __name__ == "__main__":
             # Release the VideoCapture object and close all windows
             play.release()
             cv2.destroyAllWindows()
-
-
-    # app = QApplication(sys.argv)
-    # player = VideoPlayer()
-    # player.setWindowTitle("Player")
-    # player.resize(600, 400)
-    # player.show()
-    # sys.exit(app.exec_())
